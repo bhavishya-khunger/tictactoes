1fd4ba2447020436c535adda638d369874caf76e
Bhavishya Khunger <bhavishyakhunger@gmail.com>
2025-10-19 02:36:10 +0530

    app config

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -8,7 +8,7 @@ import WelcomeScreen from './components/WelcomeScreen';
 import Lobby from './components/Lobby';
 
 // Initialize socket
-const socket = io('https://tictactoes-server.vercel.app');
+const socket = io('https://tictactoes-server.vercel.app', { transports: ['polling'] });
 
 function App() {
   const [user, setUser] = useState(null); // { id, name, avatar }
